afd8ec01f3c8dce42d0669dbb3271cf6990e9786
kelton <kelton.xian@laiyou.net>
2016-08-05 18:42:36 +0800

    update

---
+++ Assets/Scripts/PopupTargetMover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupTargetMover : MonoBehaviour {
+
+	// define the possible states through an enumeration
+	public enum motionDirections { Up, Float, UpFast };
+
+	// store the state
+	public motionDirections motionState = motionDirections.Up;
+
+	// motion parameters
+	public float motionMagnitude = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+
+		// do the appropriate motion based on the motionState
+		switch(motionState) {
+		case motionDirections.Up:
+			// rotate around the up axix of the gameObject
+			gameObject.transform.Translate(Vector3.up * motionMagnitude);
+			break;
+		case motionDirections.Float:
+			// move up and down over time
+			gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude/2);
+			break;
+		case motionDirections.UpFast:
+			// move up and down over time
+			gameObject.transform.Translate(Vector3.up * motionMagnitude * 2);
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnGameObjectsPopup.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjectsPopup : MonoBehaviour {
+
+	// public variables
+	public float secondsBetweenSpawning = 0.1f;
+	public float xAbsMinRange = 4.0f;
+	public float xAbsMaxRange = 6.0f;
+	public float yAbsMinRange = 0.0f;
+	public float yAbsMaxRange = 1.0f;
+	public float zAbsMinRange = 4.0f;
+	public float zAbsMaxRange = 6.0f;
+	public GameObject[] spawnObjects; // what prefabs to spawn
+
+	private float nextSpawnTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// determine when to spawn the next object
+		nextSpawnTime = Time.time+secondsBetweenSpawning;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// exit if there is a game manager and the game is over
+		if (GameManager.gm) {
+			if (GameManager.gm.gameIsOver)
+				return;
+		}
+
+		// if time to spawn a new game object
+		if (Time.time  >= nextSpawnTime) {
+			// Spawn the game object through function below
+			MakeThingToSpawn ();
+
+			// determine the next time to spawn the object
+			nextSpawnTime = Time.time+secondsBetweenSpawning;
+		}	
+	}
+
+	void MakeThingToSpawn ()
+	{
+		Vector3 spawnPosition;
+
+		// get a random position between the specified ranges
+		spawnPosition.x = Random.Range (0, xAbsMaxRange);
+		spawnPosition.y = Random.Range (yAbsMinRange, yAbsMaxRange);
+		spawnPosition.z = Random.Range (0, zAbsMaxRange);
+		if (spawnPosition.x < xAbsMinRange && spawnPosition.z < zAbsMinRange) {
+			return;
+		}
+		float t1 = Random.Range (-1, 1);
+		float t2 = Random.Range (-1, 1);
+		if (t1 < 0) {
+			spawnPosition.x *= -1;
+		}
+		if (t2 < 0) {
+			spawnPosition.z *= -1;
+		}
+
+		// determine which object to spawn
+		int objectToSpawn = Random.Range (0, spawnObjects.Length);
+
+		// actually spawn the game object
+		GameObject spawnedObject = Instantiate (spawnObjects [objectToSpawn], spawnPosition, transform.rotation) as GameObject;
+
+		// make the parent the spawner so hierarchy doesn't get super messy
+		spawnedObject.transform.parent = gameObject.transform;
+	}
+}
